ff7a5ee32f9183bcc45be94dd1f5e714cc6ab523
Pablo Durán <pabloduran13@gmail.com>
2021-04-20 18:40:39 +0200

    Init commit

---
+++ index.js
@@ -0,0 +1,9 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css'; // Import precompiled Bootstrap css
+import '@fortawesome/fontawesome-free/css/all.css';
+
+import 'vanilla-tilt';
+import './js/main.js';
+
+import './node_modules/aos/dist/aos.css';
+import './scss/styles.scss';
